b60634afb7391da08904c8839750ddcf55477ab0
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-04-24 11:04:22 +0200

    Add project files.

---
+++ nyslutprojekt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyslutprojekt
+{
+    class Restaurangbokning
+    {
+        private int[,] tables;
+        private int[,] bookings;
+        private int numBookings;
+
+        public Restaurangbokning()
+        {
+            tables = new int[,] { { 1, 2 }, { 2, 4 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 7, 6 }, { 8, 6 }, { 9, 8 }, { 10, 8 } };
+            bookings = new int[10, 3];
+            numBookings = 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

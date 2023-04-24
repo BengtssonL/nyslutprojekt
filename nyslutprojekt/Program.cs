a6279ad1469694b5ec7409e4785994cb5c2150af
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-04-24 23:28:59 +0200

    Fixade Showbookings metoden och la även till lite input validation i bookatable

--- nyslutprojekt/Program.cs
+++ nyslutprojekt/Program.cs
@@ -11,6 +11,7 @@ namespace nyslutprojekt
         static void Main(string[] args)
         {
             Restaurangbokning bokningssytem = new Restaurangbokning();
+            Console.Clear();
             bokningssytem.Start();
         }
     }
